a39eb66b098518df354f0013738c59a87819eb4c
matiooo <raminhacker.rm@gmail.com>
2020-07-09 09:31:23 -0700

    finish project

--- app/src/main/java/ir/hrwanheda/dbnumber/welcome/WelcomeActivity.java
+++ app/src/main/java/ir/hrwanheda/dbnumber/welcome/WelcomeActivity.java
@@ -12,6 +12,7 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import java.util.Objects;
 
@@ -23,13 +24,25 @@ import ir.hrwanheda.dbnumber.R;
 public class WelcomeActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    ImageView img_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        preferences = getSharedPreferences("pref",MODE_PRIVATE);
-
-
         setContentView(R.layout.activity_welcome);
+        preferences = getSharedPreferences("pref",MODE_PRIVATE);
+        img_logo = findViewById(R.id.activity_welcome_img_logo);
+
+
+        DisplayMetrics displayMetrics;
+        displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        img_logo.getLayoutParams().width = (int)(displayMetrics.widthPixels * 0.6);
+        img_logo.getLayoutParams().height = (int)(displayMetrics.widthPixels * 0.6);
+
+
+
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Objects.requireNonNull(getSupportActionBar()).hide();
         }
@@ -50,9 +63,8 @@ public class WelcomeActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(WelcomeActivity.this, ActivityCheckPerm.class));
-                finish();
-                /*
+
+
                 if(preferences.getString("start","").isEmpty())
                 {
                     startActivity(new Intent(WelcomeActivity.this, ActivityCheckPerm.class));
@@ -62,9 +74,34 @@ public class WelcomeActivity extends AppCompatActivity {
                     finish();
                 }
 
-                 */
 
             }
-        }, 2500);
+        }, 2000);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
 }
